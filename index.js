f4881fd42b6f4da86527f9484aeb39d562347b3e
JonathanSch <jonathansch7@gmail.com>
2021-02-18 15:53:57 -0600

    Things modified

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(require('cors')());
 require('./database');
 const routes = require('./routes')
 
