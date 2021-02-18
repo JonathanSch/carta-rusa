9925fdec5345b3989dd8d929526fb4a3a3756858
JonathanSch <jonathansch7@gmail.com>
2021-02-18 17:13:47 -0600

    Things added

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const cors = require('cors');
 
 require('./database');
 const routes = require('./routes')
 
+app.use(cors());
 app.use(express.json());
 app.use('/routes',routes)
-app.use(require('cors')());
+
 
 app.get('/', (req,res)=>{
     res.send({message:'All great'}).status(200);
